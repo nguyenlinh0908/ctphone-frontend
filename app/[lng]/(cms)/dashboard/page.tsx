68d89aca5577c5b3f89214c8783af89210f83fc6
linglv <nguyenlinh982001@gmail.com>
2023-10-13 19:29:38 +0700

    add dashboard

--- app/[lng]/(cms)/dashboard/page.tsx
+++ app/[lng]/(cms)/dashboard/page.tsx
@@ -1,7 +1,21 @@
+'use client'
+
+import { Col, Row, Statistic } from 'antd';
+import CountUp from 'react-countup';
+
 export default function Dashboard() {
+  const formatter = (value: number) => <CountUp end={value} separator="," />;
+
   return (
     <>
-      <div>dash board</div>
+      <Row gutter={16}>
+        <Col span={12}>
+          <Statistic title="Customer" value={100} formatter={() => formatter(100)} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Balance (VND)" value={1200000} precision={2} formatter={() => formatter(1200000)} />
+        </Col>
+      </Row>
     </>
   );
 }
